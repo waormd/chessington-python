c1aa240df5dea8a12d9a97f25e1a2059724646cf
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-08-01 14:14:00 +0100

    Added a test that pawns can move two squares

--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -33,3 +33,69 @@ class TestPawns:
 
         # Assert
         assert Square.at(5, 4) in moves
+
+    @staticmethod
+    def test_white_pawn_can_move_up_two_squares_if_not_moved():
+
+        # Arrange
+        board = Board.empty()
+        pawn = Pawn(Player.WHITE)
+        square = Square.at(1, 4)
+        board.set_piece(square, pawn)
+
+        # Act
+        moves = pawn.get_available_moves(board)
+
+        # Assert
+        assert Square.at(3, 4) in moves
+
+    @staticmethod
+    def test_black_pawn_can_move_down_two_squares_if_not_moved():
+
+        # Arrange
+        board = Board.empty()
+        pawn = Pawn(Player.BLACK)
+        square = Square.at(6, 4)
+        board.set_piece(square, pawn)
+
+        # Act
+        moves = pawn.get_available_moves(board)
+
+        # Assert
+        assert Square.at(4, 4) in moves
+
+    @staticmethod
+    def test_white_pawn_cannot_move_up_two_squares_if_already_moved():
+
+        # Arrange
+        board = Board.empty()
+        pawn = Pawn(Player.WHITE)
+        starting_square = Square.at(1, 4)
+        board.set_piece(starting_square, pawn)
+
+        intermediate_square = Square.at(2, 4)
+        pawn.move_to(board, intermediate_square)
+
+        # Act
+        moves = pawn.get_available_moves(board)
+
+        # Assert
+        assert Square.at(4, 4) not in moves
+
+    @staticmethod
+    def test_black_pawn_cannot_move_down_two_squares_if_already_moved():
+
+        # Arrange
+        board = Board.empty()
+        pawn = Pawn(Player.BLACK)
+        starting_square = Square.at(6, 4)
+        board.set_piece(starting_square, pawn)
+
+        intermediate_square = Square.at(5, 4)
+        pawn.move_to(board, intermediate_square)
+
+        # Act
+        moves = pawn.get_available_moves(board)
+
+        # Assert
+        assert Square.at(3, 4) not in moves
