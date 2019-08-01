cbea4853350561cfeaab6ba42c7e792d46203880
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-08-01 13:15:28 +0100

    Added some basic passing tests

---
+++ tests/test_board.py
@@ -0,0 +1,37 @@
+from chessington.engine.board import Board, Square, Player
+
+def test_new_board_has_white_pieces_at_bottom():
+
+    # Arrange
+    board = Board()
+
+    # Act
+    piece = board.get_piece(Square.at(0, 0))
+
+    # Assert
+    assert piece.player == Player.WHITE
+
+def test_new_board_has_black_pieces_at_top():
+
+    # Arrange
+    board = Board()
+
+    # Act
+    piece = board.get_piece(Square.at(7, 0))
+
+    # Assert
+    assert piece.player == Player.BLACK
+
+def test_pieces_can_be_moved_on_the_board():
+
+    # Arrange
+    board = Board()
+    from_square = Square.at(1, 0)
+    piece = board.get_piece(from_square)
+
+    # Act
+    to_square = Square.at(3, 0)
+    board.move_piece(from_square, to_square)
+
+    assert board.get_piece(from_square) is None
+    assert board.get_piece(to_square) is piece
--- tests/test_chessington.py
+++
@@ -1,5 +0,0 @@
-from chessington import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
