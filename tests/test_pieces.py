080c347ee14bb1b00f14592cb3ec79f78aee7b6a
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-08-01 13:48:55 +0100

    Added a test that pawns can move one square

---
+++ tests/test_pieces.py
@@ -0,0 +1,35 @@
+from chessington.engine.board import Board
+from chessington.engine.data import Player, Square
+from chessington.engine.pieces import Pawn
+
+class TestPawns:
+
+    @staticmethod
+    def test_white_pawns_can_move_up_one_square():
+
+        # Arrange
+        board = Board.empty()
+        pawn = Pawn(Player.WHITE)
+        square = Square.at(1, 4)
+        board.set_piece(square, pawn)
+
+        # Act
+        moves = pawn.get_available_moves(board)
+
+        # Assert
+        assert Square.at(2, 4) in moves
+
+    @staticmethod
+    def test_black_pawns_can_move_down_one_square():
+
+        # Arrange
+        board = Board.empty()
+        pawn = Pawn(Player.BLACK)
+        square = Square.at(6, 4)
+        board.set_piece(square, pawn)
+
+        # Act
+        moves = pawn.get_available_moves(board)
+
+        # Assert
+        assert Square.at(5, 4) in moves
